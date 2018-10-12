1d99ace8b3eb30ccfc8ba3418d47c0cff32c3a11
kkkksang <kkkksang@naver.com>
2018-10-13 01:35:24 +0900

    Update annyang.js

--- annyang.js
+++ annyang.js
@@ -170,7 +170,7 @@
 
       // Set the max number of alternative transcripts to try and match with a command
       recognition.maxAlternatives = 5;
-recognition.interimResults = true;
+
       // In HTTPS, turn off continuous mode for faster results.
       // In HTTP,  turn on  continuous mode for much slower results, but no repeating security notices
       recognition.continuous = root.location.protocol === 'http:';
